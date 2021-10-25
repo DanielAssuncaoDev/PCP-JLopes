4837f89e8121a5d00485d385ea521857e84513a1
rafaelladamiski <rafaelladamiski@gmail.com>
2021-10-25 16:36:17 -0300

    ok

--- Backend/api/src/index.js
+++ Backend/api/src/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import crypto from 'crypto-js' 
 import require from 'sequelize'
-import sequelize from 'sequelize'
+//import sequelize from 'sequelize'
 import { any } from 'sequelize/types/lib/operators';
 // import pcpjp2021_tb_produto from './models/pcpjp2021_tb_produto.js';
 // import pcpjp2021_tb_controle_estoque from './models/pcpjp2021_tb_controle_estoque.js';
